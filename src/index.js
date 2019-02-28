5f8b84a8d84e590f44045687f79ee48db023a3ee
antoinequellier <antoinecquellier@gmail.com>
2019-02-28 11:33:54 +0000

    using JS objects in jsx

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 // Import react and react libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 // Create a react component
 const App = () => {
-  const buttonText = 'Click me!';
+  const buttonText = { text: 'Click me!'};
+  const labelText = 'Enter name: '
   return(
     <div>
       <label className="label" for="name">
-        Enter name:
+        {labelText}
       </label>
       <input id="name" type="text"/>
       <button style={{backgroundColor: 'blue', color: 'white'}}>
-        {buttonText}
+        {buttonText.text}
       </button>
     </div>
   )
